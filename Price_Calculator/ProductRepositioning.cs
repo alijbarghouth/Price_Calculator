22ef058de2512de83e7f580f10f82a7613c2b9dd
alijbarghouth <alijbarghouth@gmail.com>
2023-03-28 13:46:04 +0300

    add Product Repo

---
+++ Price_Calculator/ProductRepositioning.cs
@@ -0,0 +1,37 @@
+﻿using Price_Calculator.ProductService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Price_Calculator
+{
+    public class ProductRepositioning
+    {
+        private readonly ITAXService _taxService;
+
+        public ProductRepositioning(ITAXService taxService)
+        {
+            _taxService = taxService;
+        }
+
+        public  decimal GetTheFinalPriceAfterTax()
+        {
+            Product product = new()
+            {
+                Name = "Test",
+                Price = 20.25m,
+                UPC = 10
+            };
+            Console.WriteLine($"The Price before Tax is :{product.Price}");
+
+
+            var TotalPrice = product.Price + _taxService.TheTaxOnThePrice(product.Price);
+
+            return Math.Round(TotalPrice, 2);
+        }
+
+        
+    }
+}
--- Price_Calculator/Program.cs
+++ Price_Calculator/Program.cs
@@ -5,38 +5,9 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        int tax = int.Parse(Console.ReadLine());    
-        Console.WriteLine($"The Price after Tax is :{GetTheFinalPriceAfterTax(tax)}");
+        ProductRepositioning productRepositioning = new ProductRepositioning(new TwentyPercentOff());
+        Console.WriteLine($"The Price after Tax is :{productRepositioning.GetTheFinalPriceAfterTax()}");
     }
 
-    private static decimal GetTheFinalPriceAfterTax(int tax)
-    {
-        Product product = new()
-        {
-            Name = "Test",
-            Price = 20.25m,
-            UPC = 10
-        };
-        Console.WriteLine($"The Price before Tax is :{product.Price}");
-
-
-        ITAXService taxType = GetTAXType(tax);
-
-        var TotalPrice = product.Price + taxType.TheTaxOnThePrice(product.Price);
-
-        return Math.Round(TotalPrice, 2);
-    }
-
-    private static ITAXService GetTAXType(int tax)
-    {
-        switch (tax)
-        {
-            case 20:
-                return new TwentyPercentOff();
-            case 30:
-                return new ThirtyPercentOff();
-            default:
-                return new TenPercentOff();
-        }
-    }
+   
 }
